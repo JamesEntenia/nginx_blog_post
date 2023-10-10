ec0f8f02e6a9c0952a5b8a05dcba251e663eb403
JamesEntenia <jamesentenia23@gmail.com>
2023-10-10 12:46:44 +0800

    new version

--- insert.php
+++ insert.php
@@ -1,26 +1,26 @@
 <?php
-$servername = "your-rds-endpoint";
-$username = "your-rds-username";
-$password = "your-rds-password";
-$dbname = "your-rds-database";
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 // Get data from the form
-$name = $_POST['name'];
-$email = $_POST['email'];
+$name = $_POST["name"];
+$email = $_POST["email"];
 
-// SQL query to insert data
+// Insert data into the database
 $sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Data inserted successfully.";
+    echo "Data inserted successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
